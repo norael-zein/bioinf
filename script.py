b5f2f8f9d3e59cf06016786ec3c413a6e215003f
smosebach <sara_1022@hotmail.com>
2023-04-17 13:31:51 +0200

    draft3

--- script.py
+++ script.py
@@ -4,7 +4,19 @@ import sys
 input_file = sys.argv[1]
 raw_df = pd.read_csv(input_file)
 df = raw_df.drop(raw_df.columns[[0,1,2,3,4,5,6,7,8,9,10,11,12]], axis=1)
-print(df.apply(pd.Series.value_counts))
+
+freq = df.apply(pd.Series.value_counts)
+positions = list(freq.columns[:])
+aminoacid = list(freq.index)
+
+for pos in positions:
+    for a in aminoacid:
+        print(freq[pos][a])
+
+
+
+#print(freq.loc[:, "2"])
+
 
 
 frequencies = {}
@@ -13,10 +25,10 @@ aminoacids = {"A":0, "R":0, "N":0, "D":0, "C":0,
               "L":0, "K":0, "M":0, "F":0, "P":0, 
               "S":0, "T":0, "W":0, "Y":0, "V":0, "*":0}
 
-positions = list(df.columns[13:])
-for pos in positions:
-    frequencies[pos] = aminoacids
+#positions = list(df.columns[13:])
+#for pos in positions:
+#    frequencies[pos] = aminoacids
 
-bajs = df[[positions[0]]].value_counts()#print(bajs.keys())
+#bajs = df[[positions[0]]].value_counts()#print(bajs.keys())
 
 
