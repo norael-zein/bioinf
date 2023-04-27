508efc000d34ec9c8ac02a3d8430894135044e4e
Nora El-Zein <nora.ez11@gmail.com>
2023-04-27 22:43:04 +0200

    Added comments

--- noraZ_2.py
+++ noraZ_2.py
@@ -6,24 +6,25 @@ input_file = sys.argv[1]
 raw = pd.read_csv(input_file)
 df = raw.drop(raw.columns[:13], axis=1)
 
+#Input positions to write in the command window:
 positions = input("Enter a comma-separated list of positions: ")
-positions = positions.split(",")
+positions = positions.split(",") 
 positions = [pos.strip() for pos in positions]  # Enable to have space between the positions in the input
-parts = {}
+parts = {} #Short sequences added here
 
-for pos in positions:
-    part = df.loc[df[pos] == '-']
-    if len(part.index) == 0:
-        print(f"No rows dropped for position {pos}")
-    else:
-        parts[pos] = part
-        df.drop(part.index, inplace=True)
-        print(f"{len(part.index)} rows dropped for position {pos}")
-        print(part)
+for pos in positions: #Iterates over selected positions
+    part = df.loc[df[pos] == '-'] #Selects all the columns where values in pos is equal to '-' and put the sequences in 'part'
+    if len(part.index) == 0: #If part is an empty dataframe, do not print it out
+        print(f"No rows dropped for position {pos}") 
+    else: #If dataframe is not empty
+        parts[pos] = part #Add part to parts dictionary 
+        df.drop(part.index, inplace=True) #Drops all rows from dataframe where the value in pos is equal to '-'
+        print(f"{len(part.index)} rows dropped for position {pos}") #Print the amount of short sequences
+        print(part) #Print short sequence
 
-print(f"{len(df.index)} rows remaining")
-print(f'Long sequences: {df}')
-print(f' List of short sequences: {parts}')
+print(f"{len(df.index)} rows remaining") #Print the amount of long sequences
+print(f'Long sequences: {df}') #Print all long sequences
+print(f' List of short sequences: {parts}') #Print all short sequences from the dictionary 
 
 #Adding short sequences to a text file
 with open('testoutputNoraZ.txt', 'w') as f:
@@ -31,4 +32,4 @@ with open('testoutputNoraZ.txt', 'w') as f:
     for key, value in parts.items():
         f.write(f'{key}: {value}\n')
 
-print(parts)
+print(parts) #Print dictionary containing all short sequences
