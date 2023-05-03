965fd4a303921a3993bb7938cea2596866274520
Nora El-Zein <nora.ez11@gmail.com>
2023-05-03 10:47:51 +0200

    CDR1 and 2, frequences

--- frequencyCDR.py
+++ frequencyCDR.py
@@ -9,39 +9,39 @@ input_file = sys.argv[1]
 raw = pd.read_csv(input_file) 
 df = raw.drop(raw.columns[:13], axis=1)
 
-# Determination of CDR3 length
+# Determination of CDR length
 counter = {'A':1,'C':1,'D':1,'E':1,'F':1,'G':1,'H':1,
            'I':1,'K':1,'L':1,'M':1,'N':1,'P':1,'Q':1,
            'R':1,'S':1,'T':1,'V':1,'W':1,'Y':1,'-':0}
 
-start = df.columns.get_loc('95')
-stop = df.columns.get_loc('102')
+start = df.columns.get_loc('50')
+stop = df.columns.get_loc('65')
 
-CDR3 = df.iloc[:,start:stop+1]
-CDR3 = CDR3.replace(counter)
-CDR3['CDR3_length'] = CDR3.sum(axis=1)          #sätt ihop denna o nästa rad
-df['CDR3_length'] = CDR3['CDR3_length']
+CDR = df.iloc[:,start:stop+1]
+CDR = CDR.replace(counter)
+CDR['CDR_length'] = CDR.sum(axis=1)          #sätt ihop denna o nästa rad
+df['CDR_length'] = CDR['CDR_length']
 
 #-----------------------------------------------------------------
-# # Plot number of sequences depending on CDR3 length
-# min = 5
-# #min = df['CDR3_length'].min()
-# max = df['CDR3_length'].max()
+# Plot number of sequences depending on CDR3 length
+min = 5
+min = df['CDR_length'].min()
+max = df['CDR_length'].max()
 
-# length_range = [*range(min,max+1,1)]
-# nr_sequences = []
-# lengths = []
-# for l in length_range:
-#     part = df.loc[df['CDR3_length'] == l]
-#     amount = len(part.index)
-#     nr_sequences.append(amount)
-#     lengths.append(l)
+length_range = [*range(min,max+1,1)]
+nr_sequences = []
+lengths = []
+for l in length_range:
+    part = df.loc[df['CDR_length'] == l]
+    amount = len(part.index)
+    nr_sequences.append(amount)
+    lengths.append(l)
 
-# sum = sum(nr_sequences)
-# occurrences = [x/sum for x in nr_sequences]
+sum = sum(nr_sequences)
+occurrences = [x/sum for x in nr_sequences]
 
-# plt.bar(lengths, occurrences)
-# plt.title('CDR3 length')
-# plt.xlabel('Number of amino acids')
-# plt.ylabel('Number of sequences')
-# plt.show()
+plt.bar(lengths, occurrences)
+plt.title('CDR2 length')
+plt.xlabel('Number of amino acids')
+plt.ylabel('Percentage of sequences')
+plt.show()
