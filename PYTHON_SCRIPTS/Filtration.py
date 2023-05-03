6b5c51035b5ddfce51f4f88a2120f2d36c0d11d7
smosebach <sara_1022@hotmail.com>
2023-05-03 13:39:08 +0200

    test

--- PYTHON_SCRIPTS/Filtration.py
+++ PYTHON_SCRIPTS/Filtration.py
@@ -4,7 +4,8 @@ import sys
 
 input_file = sys.argv[1]
 
-df = pd.read_csv(input_file) 
+raw = pd.read_csv(input_file) 
+df = raw.drop(raw.columns[:13], axis=1)
 
 filter = {'103':'-'}
 
@@ -17,11 +18,9 @@ counter = {'A':1,'C':1,'D':1,'E':1,'F':1,'G':1,'H':1,
            'I':1,'K':1,'L':1,'M':1,'N':1,'P':1,'Q':1,
            'R':1,'S':1,'T':1,'V':1,'W':1,'Y':1,'-':0}
 
-print(df['35B'])
-
-start_1 = df.columns.get_loc('30')
-start_2 = df.columns.get_loc('50')
-start_3 = df.columns.get_loc('95')
+start_1 = df.columns.get_loc('30')              # Start postion CDR1 
+start_2 = df.columns.get_loc('50')              # Start postion CDR2
+start_3 = df.columns.get_loc('95')              # Start postion CDR3
 
 stop_1 = df.columns.get_loc('35B')
 stop_2 = df.columns.get_loc('65')
@@ -41,8 +40,5 @@ df['CDR2_length'] = CDR2.sum(axis=1)
 CDR3 = df.iloc[:,start_3:stop_3+1]
 CDR3 = CDR3.replace(counter)
 df['CDR3_length'] = CDR3.sum(axis=1)
-
-# CDR3['CDR3_length'] = CDR3.sum(axis=1)          #sätt ihop denna o nästa rad
-# df['CDR3_length'] = CDR3['CDR3_length']
 
 df.to_csv('Test_filter.csv', index = False)
