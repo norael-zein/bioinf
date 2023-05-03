3a34c5c4e05ba01c6a27f205d90b927efd9eba6c
smosebach <sara_1022@hotmail.com>
2023-05-03 08:44:55 +0200

    script

--- CDR3.py
+++ CDR3.py
@@ -5,6 +5,7 @@ import sys
 
 input_file = sys.argv[1]
 
+# df = pd.read_csv(input_file) 
 raw = pd.read_csv(input_file) 
 df = raw.drop(raw.columns[:13], axis=1)
 
@@ -18,7 +19,7 @@ stop = df.columns.get_loc('102')
 
 CDR3 = df.iloc[:,start:stop+1]
 CDR3 = CDR3.replace(counter)
-CDR3['CDR3_length'] = CDR3.sum(axis=1)
+CDR3['CDR3_length'] = CDR3.sum(axis=1)          #sätt ihop denna o nästa rad
 df['CDR3_length'] = CDR3['CDR3_length']
 
 #-----------------------------------------------------------------
@@ -48,7 +49,31 @@ df['CDR3_length'] = CDR3['CDR3_length']
 #-------------------------------------------------------------------
 # Different output files
 
-pos1 =
+threshold1 = 12
+threshold2 = 18
 
-part1 = df.loc[df['CDR3_length'] = l]
+short = df.loc[df['CDR3_length'] <= threshold1]
+medium = df[ (df['CDR3_length'] > threshold1) & (df['CDR3_length'] <= threshold2)]
+long = df[ (df['CDR3_length'] > threshold2)]
 
+
+print(short.head())
+print(medium.head())
+print(long.head())
+
+# # Creation of a frequency DataFrame, listing the columns as positions 
+# # and indexes as amino acids
+# df_freq = df.apply(pd.Series.value_counts)
+# positions = list(df_freq.columns)
+# aminoacid = list(df_freq.index)
+
+# # Creation of a frequency dictionary from frequency DataFrame
+# frequencies = {'Amino_acid':['Pos'+ pos for pos in positions]}
+# for i, aa in enumerate(aminoacid):
+#     #Filling the NaN values with zeros and converts all values to integers
+#     frequencies[aa] = df_freq.iloc[i,:].fillna(0).astype(int)
+
+# # Creation of output file, adding the values in the right format
+# with open(f'frequencies_{input_file}', 'w') as output_file:
+#     lines = [f'{aa},' + ','.join([f if isinstance(f, str) else str(f) for f in freq]) for aa, freq in frequencies.items()]
+#     output_file.write('\n'.join(lines))
