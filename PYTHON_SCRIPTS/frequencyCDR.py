6f092ccdd074f8e0996a07305a03a929d43faf8d
nohe3040 <norahemmingson@gmail.com>
2023-05-05 13:42:49 +0200

    ln

--- PYTHON_SCRIPTS/frequencyCDR.py
+++ PYTHON_SCRIPTS/frequencyCDR.py
@@ -18,6 +18,10 @@ counter = {'A':1,'C':1,'D':1,'E':1,'F':1,'G':1,'H':1,
 start = df.columns.get_loc(input("Enter a comma-separated list of positions: ") )
 stop = df.columns.get_loc(input("Enter a comma-separated list of positions: ") )
 
+#CDR1: 30-35B
+#CDR2: 50-65
+#CDR3_ 95-102
+
 CDR = df.iloc[:,start:stop+1]
 CDR = CDR.replace(counter)
 CDR['CDR_length'] = CDR.sum(axis=1)          #sätt ihop denna o nästa rad
@@ -27,11 +31,10 @@ df['CDR_length'] = CDR['CDR_length']
 # Plot number of sequences depending on CDR3 length
 
 min = df['CDR_length'].min()
-max = df['CDR_length'].max()
-print(min)
-print(max)
+max_ = df['CDR_length'].max()
 
-length_range = [*range(min,max+1,1)]
+
+length_range = [*range(min,max_+1,1)]
 nr_sequences = []
 lengths = []
 for l in length_range:
@@ -40,14 +43,22 @@ for l in length_range:
     nr_sequences.append(amount)
     lengths.append(l)
 
+
 sum = sum(nr_sequences)
 occurrences = [(x/sum)*100 for x in nr_sequences]
+max_value = max(occurrences)
+print(max_value)
 
 fig = plt.figure (figsize = (10,5))
 plt.bar(lengths, occurrences, color = 'firebrick', width = 0.5)
 plt.xticks(np.arange(0, 31, step=1))
-plt.xlim([0, 32])
-plt.title('CDR2 length distribution', weight='bold', fontsize = 16)
-plt.xlabel('CDR2 length (aa)')
+plt.xlim([0, 30])
+plt.title('CDR3 length distribution', weight='bold', fontsize = 14)
+plt.xlabel('CDR3 length (aa)')
 plt.ylabel('Occurrences (%)')
 plt.show()
+
+#CDR1: width= 0.5, range(0,15)
+#CDR2: width=0.5, range(11,26)
+#CDR3: width= 0.5, range(0,30)
+
